ef78e9b6471d27fabd33bcbb4630840227e408bc
chosh1179 <chosh@fitfuns.com>
2022-11-28 18:24:31 +0900

    download from playcanvas

--- __loading__.js
+++ __loading__.js
@@ -1,38 +1,38 @@
 pc.script.createLoadingScreen(function (app) {
-  var showSplash = function () {
-    // splash wrapper
-    var wrapper = document.createElement("div");
-    wrapper.id = "application-splash-wrapper";
-    document.body.appendChild(wrapper);
+    var showSplash = function () {
+        // splash wrapper
+        var wrapper = document.createElement("div");
+        wrapper.id = "application-splash-wrapper";
+        document.body.appendChild(wrapper);
 
-    // splash
-    var splash = document.createElement("div");
-    splash.id = "application-splash";
-    wrapper.appendChild(splash);
-    splash.style.display = "flex";
-    splash.style.flexDirection = "column";
-    splash.style.alignItems = "center";
-    splash.style.gap = "16px";
-    var icons = document.createElement("div");
-    icons.id = "application-icons";
-    icons.style.display = "flex";
-    icons.style.height = "50px";
-    splash.appendChild(icons);
-    var logo = document.createElement("div");
-    logo.id = "logo";
-    logo.innerHTML = `<svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+        // splash
+        var splash = document.createElement("div");
+        splash.id = "application-splash";
+        wrapper.appendChild(splash);
+        splash.style.display = "flex";
+        splash.style.flexDirection = "column";
+        splash.style.alignItems = "center";
+        splash.style.gap = "16px";
+        var icons = document.createElement("div");
+        icons.id = "application-icons";
+        icons.style.display = "flex";
+        icons.style.height = "50px";
+        splash.appendChild(icons);
+        var logo = document.createElement("div");
+        logo.id = "logo";
+        logo.innerHTML = `<svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M6.4475 39.5964L21.6287 48.4602C22.6752 49.0705 23.8648 49.3921 25.0763 49.3921C26.2877 49.3921 27.4773 49.0705 28.5238 48.4602L43.705 39.5964C44.7558 38.9775 45.6266 38.0949 46.2313 37.0359C46.836 35.9769 47.1535 34.7784 47.1525 33.5589V15.8314C47.1535 14.612 46.836 13.4134 46.2313 12.3544C45.6266 11.2954 44.7558 10.4128 43.705 9.79391L28.5238 0.938913C27.4788 0.32417 26.2886 0 25.0763 0C23.8639 0 22.6736 0.32417 21.6287 0.938913L6.4475 9.79391C5.39675 10.4128 4.5259 11.2954 3.92121 12.3544C3.31652 13.4134 2.99897 14.612 3 15.8314V33.5589C2.99897 34.7784 3.31652 35.9769 3.92121 37.0359C4.5259 38.0949 5.39675 38.9775 6.4475 39.5964Z" fill="white"/>
 <path d="M27.261 9.30502C29.27 9.0931 27.617 15.3913 28.3121 19.3838C29.0072 23.3763 31.5163 21.0791 34.907 21.6047C43.1209 23.0712 28.3121 36.778 25.2775 39.2362C22.2428 41.6945 21.802 38.8209 21.8868 38.2784C21.9716 37.7358 23.1159 28.2335 20.2677 27.5384C18.1147 27.4536 14.3341 29.115 13.0965 26.2499C11.8419 20.2569 23.2176 11.6107 24.4383 10.5172C25.1652 9.86236 26.0716 9.44017 27.0406 9.30502" fill="#DBE1FF"/>
 </svg>
 
 `;
-    logo.style.position = "relative";
-    logo.style.left = "25px";
-    icons.appendChild(logo);
+        logo.style.position = "relative";
+        logo.style.left = "25px";
+        icons.appendChild(logo);
 
-    var bar = document.createElement("div");
-    bar.id = "progress-bar";
-    bar.innerHTML = `<svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+        var bar = document.createElement("div");
+        bar.id = "progress-bar";
+        bar.innerHTML = `<svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M6.4475 39.5964L21.6287 48.4602C22.6752 49.0705 23.8648 49.3921 25.0763 49.3921C26.2877 49.3921 27.4773 49.0705 28.5238 48.4602L43.705 39.5964C44.7558 38.9775 45.6266 38.0949 46.2313 37.0359C46.836 35.9769 47.1535 34.7784 47.1525 33.5589V15.8314C47.1535 14.612 46.836 13.4134 46.2313 12.3544C45.6266 11.2954 44.7558 10.4128 43.705 9.79391L28.5238 0.938913C27.4788 0.32417 26.2886 0 25.0763 0C23.8639 0 22.6736 0.32417 21.6287 0.938913L6.4475 9.79391C5.39675 10.4128 4.5259 11.2954 3.92121 12.3544C3.31652 13.4134 2.99897 14.612 3 15.8314V33.5589C2.99897 34.7784 3.31652 35.9769 3.92121 37.0359C4.5259 38.0949 5.39675 38.9775 6.4475 39.5964Z" fill="white"/>
 <path d="M27.261 9.30502C29.27 9.0931 27.617 15.3913 28.3121 19.3838C29.0072 23.3763 31.5163 21.0791 34.907 21.6047C43.1209 23.0712 28.3121 36.778 25.2775 39.2362C22.2428 41.6945 21.802 38.8209 21.8868 38.2784C21.9716 37.7358 23.1159 28.2335 20.2677 27.5384C18.1147 27.4536 14.3341 29.115 13.0965 26.2499C11.8419 20.2569 23.2176 11.6107 24.4383 10.5172C25.1652 9.86236 26.0716 9.44017 27.0406 9.30502" fill="url(#paint0_linear_1107_92337)"/>
 <defs>
@@ -43,11 +43,11 @@ pc.script.createLoadingScreen(function (app) {
 </defs>
 </svg>
 `;
-    bar.style.position = "relative";
-    bar.style.right = "25px";
-    icons.appendChild(bar);
-    var shinamon = document.createElement("div");
-    shinamon.innerHTML = `<svg width="137" height="27" viewBox="0 0 137 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+        bar.style.position = "relative";
+        bar.style.right = "25px";
+        icons.appendChild(bar);
+        var shinamon = document.createElement("div");
+        shinamon.innerHTML = `<svg width="137" height="27" viewBox="0 0 137 27" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M37.132 8.15889V19.3239C37.132 19.7633 36.9581 20.1848 36.6482 20.4964C36.3383 20.8079 35.9177 20.9841 35.4783 20.9864H35.2683C35.0504 20.9864 34.8346 20.9433 34.6334 20.8597C34.4322 20.776 34.2495 20.6534 34.0958 20.4989C33.9421 20.3444 33.8205 20.1611 33.7379 19.9594C33.6553 19.7578 33.6134 19.5418 33.6145 19.3239V7.85263C33.6145 7.41403 33.7888 6.9934 34.0989 6.68326C34.409 6.37312 34.8297 6.19888 35.2683 6.19888H35.4783C35.9169 6.19888 36.3375 6.37312 36.6477 6.68326C36.9578 6.9934 37.132 7.41403 37.132 7.85263V8.15889Z" fill="white"/>
 <path d="M36.1433 0.458881C35.747 0.313003 35.3172 0.283973 34.9049 0.375248C34.4926 0.466523 34.1152 0.674259 33.8175 0.973766C33.5199 1.27327 33.3145 1.65195 33.2257 2.06479C33.137 2.47763 33.1687 2.90727 33.317 3.30263C33.4176 3.60516 33.5868 3.8803 33.8115 4.10649C34.0362 4.33268 34.3102 4.50379 34.612 4.60639C35.0085 4.75428 35.4392 4.7854 35.8529 4.69607C36.2666 4.60674 36.646 4.40071 36.9461 4.10237C37.2463 3.80403 37.4547 3.42589 37.5466 3.01276C37.6384 2.59963 37.61 2.16882 37.4645 1.77138C37.3795 1.45555 37.2126 1.1678 36.9805 0.937303C36.7485 0.706806 36.4596 0.541757 36.1433 0.458881Z" fill="white"/>
 <path d="M117.614 6.97764C116.852 6.26992 115.961 5.71454 114.989 5.34139C114.028 4.96915 113.01 4.76492 111.979 4.73764C110.913 4.75294 109.862 4.98827 108.891 5.42888C107.913 5.81693 107.032 6.41423 106.309 7.17888C105.553 7.94171 104.957 8.84941 104.559 9.84764C104.167 10.7917 103.982 11.8094 104.017 12.8314C103.989 13.8928 104.196 14.9472 104.621 15.9201C105.016 16.8917 105.565 17.7929 106.248 18.5889C107.026 19.3409 107.929 19.9509 108.917 20.3914C109.891 20.8125 110.945 21.0184 112.006 20.9951C113.124 21.0197 114.234 20.8143 115.269 20.3914C116.268 20.017 117.155 19.3971 117.851 18.5889C118.615 17.8659 119.212 16.985 119.601 16.0076C119.988 14.9626 120.193 13.8585 120.204 12.7439C120.181 11.6759 119.946 10.6232 119.513 9.64639C119.095 8.62216 118.445 7.70872 117.614 6.97764ZM115.287 16.1739C114.433 16.9847 113.305 17.444 112.128 17.4602C110.951 17.4764 109.81 17.0484 108.934 16.2614C108.106 15.3126 107.673 14.0814 107.727 12.8226C107.703 11.6186 108.134 10.4498 108.934 9.55014C109.346 9.12801 109.841 8.79578 110.388 8.5744C110.935 8.35303 111.521 8.2473 112.111 8.26389C112.692 8.25714 113.268 8.36953 113.804 8.59412C114.34 8.81872 114.824 9.15076 115.227 9.56974C115.629 9.98872 115.942 10.4858 116.145 11.0302C116.348 11.5746 116.437 12.1549 116.407 12.7351C116.49 13.9828 116.089 15.2146 115.287 16.1739Z" fill="white"/>
@@ -59,130 +59,127 @@ pc.script.createLoadingScreen(function (app) {
 <path d="M7.38197 11.3614C5.32572 11.0114 4.29322 10.8451 4.37197 9.81263C4.45072 8.78013 6.00823 8.52638 6.95323 8.52638C7.89823 8.52638 9.27198 8.75387 9.72698 9.58512C9.88116 9.85218 10.1032 10.0738 10.3705 10.2274C10.6379 10.3811 10.9411 10.4613 11.2495 10.4601C11.4632 10.4695 11.6768 10.4367 11.8778 10.3634C12.0789 10.2902 12.2635 10.1781 12.4212 10.0334C12.5789 9.88873 12.7065 9.7144 12.7967 9.52037C12.8869 9.32633 12.938 9.11641 12.947 8.90262C12.9458 8.66523 12.8889 8.43144 12.7807 8.22012C12.7451 8.15281 12.7041 8.08844 12.6582 8.02762C11.7132 6.27762 9.67447 5.21887 7.04072 5.15762C3.43572 5.15762 0.915722 6.90763 0.766972 9.62013C0.425722 13.7501 5.32572 14.4326 6.89197 14.6951C8.95697 14.9489 10.077 15.3776 10.2432 16.0689C10.4182 17.1889 9.21072 17.6176 7.24197 17.6176C5.43072 17.6176 4.54697 17.0226 4.12697 16.4889C3.97223 16.3003 3.77709 16.1489 3.55597 16.0459C3.33484 15.9429 3.09339 15.891 2.84948 15.8939H2.51697C2.19865 15.8964 1.88753 15.9889 1.61949 16.1606C1.35145 16.3323 1.13741 16.5763 1.00204 16.8644C0.866669 17.1526 0.815483 17.4731 0.854397 17.789C0.893312 18.105 1.02074 18.4035 1.22198 18.6501C2.49073 20.1551 4.80072 20.9076 7.11072 20.9689C11.6607 20.9689 13.7257 18.6501 13.8132 16.3226C13.892 12.7351 10.4795 11.7901 7.38197 11.3614Z" fill="white"/>
 </svg>
 `;
-    splash.appendChild(shinamon);
-  };
+        splash.appendChild(shinamon);
+    };
 
-  var hideSplash = function () {
-    var splash = document.getElementById("application-splash-wrapper");
-    splash.parentElement.removeChild(splash);
-  };
+    var hideSplash = function () {
+        var splash = document.getElementById("application-splash-wrapper");
+        splash.parentElement.removeChild(splash);
+    };
 
-  var setProgress = function (value) {
-    var bar = document.getElementById("progress-bar");
-    if (bar) {
-      value = 100 - Math.min(1, Math.max(0, value)) * 100;
-      bar.style.clipPath = `polygon(0 ${value}%, 100% ${value}%, 100% 100%, 0% 100%)`;
-    }
-  };
+    var setProgress = function (value) {
+        var bar = document.getElementById("progress-bar");
+        if (bar) {
+            value = 100 - Math.min(1, Math.max(0, value)) * 100;
+            bar.style.clipPath = `polygon(0 ${value}%, 100% ${value}%, 100% 100%, 0% 100%)`;
+        }
+    };
 
-  var showCashing = function () {
-    var wrapper = document.createElement("div");
-    var first = document.createElement("div");
-    var second = document.createElement("div");
-    wrapper.id = "cash-message";
-    first.id = "cash-first";
-    second.id = "cash-second";
-    document.body.appendChild(wrapper);
-    wrapper.appendChild(first);
-    wrapper.appendChild(second);
-    first.innerText = `시나몬에 처음 오신 분이라면 시간이 조금 더 걸려요.`;
-    second.innerText = `조금만 기다려 주세요!`;
-  };
-  var hideCash = function () {
-    var splash = document.getElementById("cash-message");
-    if (!splash) return;
-    splash.parentElement.removeChild(splash);
-  };
+    var showCashing = function () {
+        var wrapper = document.createElement("div");
+        var first = document.createElement("div");
+        var second = document.createElement("div");
+        wrapper.id = "cash-message";
+        first.id = "cash-first";
+        second.id = "cash-second";
+        document.body.appendChild(wrapper);
+        wrapper.appendChild(first);
+        wrapper.appendChild(second);
+        first.innerText = `시나몬에 처음 오신 분이라면 시간이 조금 더 걸려요.`;
+        second.innerText = `조금만 기다려 주세요!`;
+    };
+    var hideCash = function () {
+        var splash = document.getElementById("cash-message");
+        if (!splash) return;
+        splash.parentElement.removeChild(splash);
+    };
 
-  var createCss = function () {
-    var css = [
-      "body {",
-      "    background: linear-gradient(186.09deg, #5A39FF 0%, #0046FF 95.18%);",
-      "}",
-      "",
-      "#cash-message {",
-      "position: absolute;",
-      "width: 100%;",
-      "display: flex;",
-      "flex-direction: column;",
-      "align-items: center;",
-      "bottom: 16px;",
-      "left: 50%;",
-      "color: #ffffff;",
-      "font-size: 14px;",
-      "line-height: 24px;",
-      "transform: translateX(-50%)",
-      "}",
-      "#cash-second {",
-      "font-weight: bold",
-      "}",
-      "#application-splash-wrapper {",
-      "    position: absolute;",
-      "    top: 0;",
-      "    left: 0;",
-      "    height: 100%;",
-      "    width: 100%;",
-      "    background: linear-gradient(186.09deg, #5A39FF 0%, #0046FF 95.18%);",
-      "}",
-      "",
-      "#application-splash {",
-      "    position: absolute;",
-      "    top: calc(50% - 28px);",
-      "    width: 264px;",
-      "    left: calc(50% - 132px);",
-      "}",
-      "",
-      "#logo {",
-      "}",
-      "#progress-bar-container {",
-      "    margin: 20px auto 0 auto;",
-      "    height: 2px;",
-      "    width: 100%;",
-      "    background-color: #1d292c;",
-      "}",
-      "",
-      "#progress-bar{",
-      "    width: 100%;",
-      "    height: 100%;",
-      "    overflow: hidden;",
-      "    clip-path: polygon(0 100%, 100% 100%, 100% 100%, 0% 100%);",
-      "}",
-      "",
-      "@media (max-width: 480px) {",
-      "    #application-splash {",
-      "        width: 170px;",
-      "        left: calc(50% - 85px);",
-      "    }",
-      "}",
-    ].join("\n");
+    var createCss = function () {
+        var css = [
+            "body {",
+            "    background: linear-gradient(186.09deg, #5A39FF 0%, #0046FF 95.18%);",
+            "}",
+            "",
+            "#cash-message {",
+            "position: absolute;",
+            "width: 100%;",
+            "display: flex;",
+            "flex-direction: column;",
+            "align-items: center;",
+            "bottom: 16px;",
+            "left: 50%;",
+            "color: #ffffff;",
+            "font-size: 14px;",
+            "line-height: 24px;",
+            "transform: translateX(-50%)",
+            "}",
+            "#cash-second {",
+            "font-weight: bold",
+            "}",
+            "#application-splash-wrapper {",
+            "    position: absolute;",
+            "    top: 0;",
+            "    left: 0;",
+            "    height: 100%;",
+            "    width: 100%;",
+            "    background: linear-gradient(186.09deg, #5A39FF 0%, #0046FF 95.18%);",
+            "}",
+            "",
+            "#application-splash {",
+            "    position: absolute;",
+            "    top: calc(50% - 28px);",
+            "    width: 264px;",
+            "    left: calc(50% - 132px);",
+            "}",
+            "",
+            "#logo {",
+            "}",
+            "#progress-bar-container {",
+            "    margin: 20px auto 0 auto;",
+            "    height: 2px;",
+            "    width: 100%;",
+            "    background-color: #1d292c;",
+            "}",
+            "",
+            "#progress-bar{",
+            "    width: 100%;",
+            "    height: 100%;",
+            "    overflow: hidden;",
+            "    clip-path: polygon(0 100%, 100% 100%, 100% 100%, 0% 100%);",
+            "}",
+            "",
+            "@media (max-width: 480px) {",
+            "    #application-splash {",
+            "        width: 170px;",
+            "        left: calc(50% - 85px);",
+            "    }",
+            "}",
+        ].join("\n");
 
-    var style = document.createElement("style");
-    style.type = "text/css";
-    if (style.styleSheet) {
-      style.styleSheet.cssText = css;
-    } else {
-      style.appendChild(document.createTextNode(css));
-    }
+        var style = document.createElement("style");
+        style.type = "text/css";
+        if (style.styleSheet) {
+            style.styleSheet.cssText = css;
+        } else {
+            style.appendChild(document.createTextNode(css));
+        }
 
-    document.head.appendChild(style);
-  };
+        document.head.appendChild(style);
+    };
 
-  createCss();
-  showSplash();
-  let timer = setTimeout(() => {
-    showCashing();
-  }, 1500);
-  app.on("preload:end", function () {
-    clearTimeout(timer);
-    app.off("preload:progress");
-    window.parent.postMessage(
-      {
-        type: "loaded",
-      },
-      "*"
-    );
-  });
-  app.on("preload:progress", setProgress);
-  app.on("start", hideSplash);
-  app.on("start", hideCash);
+    createCss();
+    showSplash();
+    let timer = setTimeout(() => {
+        showCashing();
+    }, 1500);
+    app.on("preload:end", function () {
+        clearTimeout(timer);
+        app.off("preload:progress");
+        window.parent.postMessage({
+            type: "loaded",
+        });
+    });
+    app.on("preload:progress", setProgress);
+    app.on("start", hideSplash);
+    app.on("start", hideCash);
 });
--- files/assets/96083043/1/data-ref.js
+++
@@ -1,8 +0,0 @@
-class DataRef extends pc.ScriptType { }
-
-pc.registerScript(DataRef, 'dataRef');
-
-DataRef.attributes.add("select_char", {
-    type: 'json', schema: [{ name: 'model', type: 'asset', assetType: 'model' }
-    ], array: true
-});
